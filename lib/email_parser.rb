86ddeb9a2f3d6914b7a85ae7354741e2dda0818f
Precious Humphrey <phumphrey23@gmail.com>
2018-05-29 00:56:00 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,12 +7,11 @@ class EmailParser
   attr_accessor :emails
 
   def initialize(emails)
-        @emails = emails
+   @emails = emails
   end
 
- def parse(emails)
-   array = @emails.split(/[\s,]/)
-   array
- end
+  def parse(emails)
+   @emails.split(/[\s,]/)
+  end
 
 end
